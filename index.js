c02ccb48094ff91ea94ffc2c8d7096d0d81b944e
Jeff Simon <jeffsbillinfo@gmail.com>
2019-11-15 03:36:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 function shout(string){
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
+function whisper(string)
+{
+  return string.toLowerCase();
+}
+
+function logShout(string)
+{
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string)
+{
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string)
+{
+  if(string.toUpperCase() === true)
+  {
+    return "YES INDEED";
+  }
+}
